29ae205108e5a81ab846153efcfe4ba0e15ddf02
billygr <billygr@users.noreply.github.com>
2022-01-27 16:09:03 +0200

    First part, Linux VM is deployed succesfully

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dvwa-on-azure
 Terraform setup for dvwa on Azure
+
+# Requirements
+
+- Terraform is installed and in the current \$PATH
+- Azure cli tools is installed and in the current \$PATH (https://docs.microsoft.com/en-us/cli/azure/)
+
+# Setup
+
+Login to your azure infrastructure with az login
+
+terraform init
+terraform plan
+terraform apply
+
---
+++ install_dvwa.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+apt update
+
+# Install prerequisites for DVWA 
+apt-get -y install apache2 mariadb-server php php-mysqli php-gd libapache2-mod-php
+
+# Database
+sudo mysqladmin create dvwa
+
+sudo mysql dvwa << EOF
+create user dvwa@localhost identified by 'p@ssw0rd';
+grant all on dvwa.* to dvwa@localhost;
+flush privileges;
+EOF
+
+cd /var/www/html
+sudo rm /var/www/html/index.html
+sudo git clone https://github.com/digininja/DVWA.git .
+sudo cp config/config.inc.php.dist config/config.inc.php
+sudo chown www-data:www-data /var/www/html/hackable/uploads/
+sudo chown www-data:www-data /var/www/html/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt
+sudo chown www-data:www-data /var/www/html/config
+
+# PHP changes for dvwa
+sudo sed -i 's/allow_url_include = Off/allow_url_include = On/g' /etc/php/7.2/apache2/php.ini
+sudo apache2ctl restart
